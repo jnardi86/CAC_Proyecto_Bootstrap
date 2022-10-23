aa243a24b1859c0ba57083289c1dada14b96395f
jnardi86 <julian.nardi@gmail.com>
2022-10-23 16:31:40 -0300

    Se agrega funcionalidad al boton Borrar del form de comprar titecket-Se modifica función de calcular precio por categoria

--- js/buyTicket.js
+++ js/buyTicket.js
@@ -1,7 +1,12 @@
 const form = document.querySelector('.form-tickets');
 
+
+// *************Precio por Categoria******************//
 form.addEventListener('submit', event => {
     event.preventDefault();
+    //Limpiar el total a pagar
+    document.querySelector('.precioTotal').textContent = "Total a pagar:";
+
     const cantidad = document.querySelector('#cantidad');
     const cant_numero = Number(cantidad.value);
     // console.log(cant_numero);
@@ -9,21 +14,30 @@ form.addEventListener('submit', event => {
     // console.log(categoria)
     const precio = 200;
 
-    // Precio por categoria
+    //Calculo descuento por categoria y lo agrego al contenido del span Precio Total
     if (categoria.value == "1") {
-        let resultado = cant_numero * (precio - (precio * 0.8));
-        const precioTotal = document.querySelector('.precioTotal p');
+        // let resultado = cant_numero * (precio - (precio * 0.8));
+        let resultado = cant_numero * (precio * 0.2);
+        const precioTotal = document.querySelector('.precioTotal');
         precioTotal.textContent += '$ ' + resultado;
     }
     else if (categoria.value == "2") {
-        let resultado = cant_numero * (precio - (precio * 0.5));
-        const precioTotal = document.querySelector('.precioTotal p');
+        // let resultado = cant_numero * (precio - (precio * 0.5));
+        let resultado = cant_numero * (precio * 0.5);
+        const precioTotal = document.querySelector('.precioTotal');
         precioTotal.textContent += '$ ' + resultado;
     }
     else {
-        let resultado = cant_numero * (precio - (precio * 0.15));
-        const precioTotal = document.querySelector('.precioTotal p');
+        // let resultado = cant_numero * (precio - (precio * 0.15));
+        let resultado = cant_numero * (precio * 0.85);
+        const precioTotal = document.querySelector('.precioTotal');
         precioTotal.textContent += '$ ' + resultado;
     }
+});
+
+// *************Borrar Total******************//
+const borrarTotal = document.querySelector('#borrarTotal');
+borrarTotal.addEventListener('click', event => {
+    document.querySelector('.precioTotal').textContent = "Total a pagar:";
 });
 
